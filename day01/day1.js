1206a2b785e3f042eee18a4a0f9ecc4f4f25b216
joe bumbaca <joebumbaca@gmail.com>
2020-12-04 15:28:00 -0800

    Add optimized solution for Day1 Part2

--- day01/day1.js
+++ day01/day1.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const text = fs.readFileSync('./input.txt', 'utf-8');
 let input = text.split('\n').map(el => { return parseInt(el) });
 
-// Part 1 Brute Force
+// Part 1 Brute Force O(n^2)
 // for (let i = 0; i < input.length; i++) {
 //   let num1 = input[i];
 //   for (let j = i + 1; j < input.length; j++) {
@@ -13,7 +13,7 @@ let input = text.split('\n').map(el => { return parseInt(el) });
 //   }
 // }
 
-// Part 1 Optimized
+// Part 1 Optimized O(n)
 const findSum = (arr) => {
   let nums = {};
   arr.forEach(num => {
@@ -28,7 +28,7 @@ const findSum = (arr) => {
 // findSum(input);
 
 
-// Part 2 Brute Force
+// Part 2 Brute Force O(n^3)
 const findSum2 = (input) => {
   for (let i = 0; i < input.length; i++) {
     let num1 = input[i];
@@ -45,3 +45,29 @@ const findSum2 = (input) => {
 };
 
 // findSum2(input);
+
+// Part2 Optimized O(n^2)
+
+const findSum3 = (input) => {
+  const hash = {};
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      let sum = input[i] + input[j];
+      if (!hash[sum]) {
+        hash[sum] = [input[i], input[j]];
+      }
+    }
+  }
+
+  for (let k = 0; k < input.length; k++) {
+    let difference = 2020 - input[k];
+    if (hash[difference]) {
+      console.log({num1: input[k], num2: hash[difference][0], num3: hash[difference][1], output: (input[k] * hash[difference][0] * hash[difference][1])})
+      return input[k] * hash[difference][0] * hash[difference][1];
+    }
+  }
+
+  return null;
+}
+
+findSum3(input);
